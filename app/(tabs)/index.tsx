bb5ebeefb509020c3138c86d331d9e3a4e7bdf89
hamalnabin <nabin.hamal66@gmail.com>
2024-10-07 08:57:44 +0545

    product result page added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,27 @@
-import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { router, Stack } from 'expo-router';
+import { useState } from 'react';
+import {  Pressable, Text, TextInput, View } from 'react-native';
 
-import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
+  const [search,setSearch] = useState("")
+  const performSearch =() =>{
+    console.warn("Search:",search)
+    router.push("/search")
+  }
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
+      <Stack.Screen options={{ title: 'Search'  }} />
+      <View className='flex-row gap-3 p-3  '>
+     <TextInput 
+     value={search}
+     onChangeText={setSearch}
+     placeholder='Search for a Product' className=' flex-1 rounded-md p-3 bg-white border border-gray-300'/>
+     <Pressable onPress={performSearch} className='bg-teal-500 p-3 rounded-md'>
+      <Text>Search</Text>
+     </Pressable>
+     </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
